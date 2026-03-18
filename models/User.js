9466a173dc34fb12714d14881220dda09b5111e0
babakaana <samiulhaq744@gmail.com>
2026-03-18 20:24:48 +0500

    added User model with validation and password hashing

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters']
+    }
+
+}, {timestamps: true})
+
+userSchema.pre('save', async function(next){
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+module.exports = mongoose.model('User', userSchema);
