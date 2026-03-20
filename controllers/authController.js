0d15fc29cea33f88a2f57b286f60cf38798256c0
babakaana <samiulhaq744@gmail.com>
2026-03-20 19:54:32 +0500

    added auth controller with signup, login, and getMe

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/AppError');
+const bcrypt = require('bcrypt');
 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '7d'});
@@ -28,3 +29,36 @@ const signup = async (req,res) => {
     })
 
 }
+
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+    if (!user) throw new AppError('User not found', 400);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new AppError('Invalid Credentials', 400);
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        }
+    })
+}
+
+const getMe = async (req, res) => {
+    const user = await User.findById(req.user).select('-password');
+
+    res.status(200).json({
+        status: 'success',
+        user
+    })
+}
+
+module.exports = {signup, login, getMe}
